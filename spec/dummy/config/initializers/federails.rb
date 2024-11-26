e4bcbfb2c6d53180630aa47351ccca92484de76d
James Smith <james@floppy.org.uk>
2024-11-26 15:38:00 +0000

    configure dummy app in initializer instead of yaml

--- spec/dummy/config/initializers/federails.rb
+++ spec/dummy/config/initializers/federails.rb
@@ -1,1 +1,15 @@
-Federails.config_from 'federails'
+# frozen_string_literal: true
+
+Federails.configure do |conf|
+  conf.app_name = "Dummy"
+  conf.app_version = "0.1"
+
+  conf.site_host = "localhost"
+  conf.site_port = "3000"
+  conf.force_ssl = false
+
+  conf.enable_discovery = true
+  conf.open_registrations = true
+  conf.server_routes_path = "federation"
+  conf.client_routes_path = nil
+end
