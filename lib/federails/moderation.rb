2d379fdf1d7e5ac36b880ba16312b4d406280301
James Smith <james@floppy.org.uk>
2024-11-26 16:23:06 +0000

    move federails include into engine definition file

--- lib/federails/moderation.rb
+++ lib/federails/moderation.rb
@@ -1,9 +1,7 @@
-require "federails"
 require "federails/moderation/version"
 require "federails/moderation/engine"
 
 module Federails
   module Moderation
-    # Your code goes here...
   end
 end
--- lib/federails/moderation/engine.rb
+++ lib/federails/moderation/engine.rb
@@ -1,6 +1,9 @@
+require "federails"
+
 module Federails
   module Moderation
     class Engine < ::Rails::Engine
+      isolate_namespace Federails::Moderation
     end
   end
 end
