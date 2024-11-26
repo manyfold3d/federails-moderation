cb4ac273b5dc82ed157bb5b47b01b1afd88eacfa
James Smith <james@floppy.org.uk>
2024-11-26 15:49:36 +0000

    add test for inbox receiving Flag successfully

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "Reports" do
   let(:report_body) {
     <<-EOF
       {
-      "@context":"https://www.w3.org/ns/activitystreams",
+        "@context":"https://www.w3.org/ns/activitystreams",
         "id":"https://mastodon.me.uk/d977f377-214b-4efd-8a47-70a37fb7ea20",
         "type":"Flag",
         "actor":"https://mastodon.me.uk/actor",
@@ -14,6 +14,11 @@ RSpec.describe "Reports" do
       }
     EOF
   }
+
+  it "should accept Flag activities" do
+    post user.actor.inbox_url, params: report_body, headers: { content_type: "application/activity" }
+    expect(response.status).to be :created
+  end
 
   it "should call registered Report handler when a flag is received by Federails" do
     expect(Federails::Moderation::Report).to receive(:handle_flag).once.and_call_original
