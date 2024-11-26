1236141cad2a3f0dfc2e35f092e4f043f188d9d7
James Smith <james@floppy.org.uk>
2024-11-26 14:37:34 +0000

    update version number spec

---
+++ spec/federails/moderation_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Federails::Moderation do
+  it "should have a version number" do
+    expect(described_class::VERSION).to be_present
+  end
+end
--- spec/federails/moderation_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Federails::ModerationTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Federails::Moderation::VERSION
-  end
-end
