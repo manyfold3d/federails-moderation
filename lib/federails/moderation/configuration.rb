702cdb88d14a67971f0c357d1e7e8df95d7531c6
James Smith <james@floppy.org.uk>
2025-03-26 14:47:49 +0000

    remove redundant (and misnamed) initialization

--- lib/federails/moderation/configuration.rb
+++ lib/federails/moderation/configuration.rb
@@ -6,7 +6,6 @@ module Federails::Moderation
     # A proc that is called any time new report is created
     # Useful for sending notifications
     mattr_accessor :on_report_created
-    @@on_report_received = nil
   end
   # rubocop:enable Style/ClassVars
 end
